0320f23d3c89c37acb81ff196f7e8a53638f269c
Kohei Mitsui <k-mitsui@sstn.jp>
2026-02-05 14:58:31 +0900

    受信プログラムと送信プログラムをそれぞれ単体にした

--- recieveprogram/recieveprogram.ino
+++ recieveprogram/recieveprogram.ino
@@ -16,6 +16,15 @@ const char* scriptURL = "https://script.google.com/macros/s/AKfycbySw8R1D5C2Bbse
 const unsigned long POLL_MS = 2000;
 unsigned long lastPoll = 0;
 
+// ボタンプログラム用
+int current_threshold = 30;
+int AButtonSum = 40;
+int BButtonSum = 0;
+
+bool flashflag(int ButtonSum) {
+  return ButtonSum >= current_threshold;
+}
+
 int extractInt(const String& json, const char* key) {
   String pat = String("\"") + key + "\":";
   int i = json.indexOf(pat);
@@ -83,5 +92,28 @@ void loop() {
     }
   }
 
+  // Aボタンの判定
+  if (flashflag(AButtonSum)) {
+    showCenterText("red", TFT_RED);
+    current_threshold += 10; 
+  }
+  // Bボタンの判定
+  if (flashflag(BButtonSum)) {
+    showCenterText("blue", TFT_BLUE);
+    current_threshold += 10;
+  }
+
   delay(10);
+}
+
+// 中央に文字を表示するための専用関数
+void showCenterText(const char* text, uint16_t color) {
+  M5.Lcd.clear();
+  M5.Lcd.setTextColor(color);
+  
+  // 画面の中心（160, 120）にテキストを表示
+  M5.Lcd.drawString(text, 160, 120);
+  
+  delay(5000); // 5秒待機
+  M5.Lcd.clear();
 }
